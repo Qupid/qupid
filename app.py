44d2e1d26f677c929d7edfe4138b20985a16662b
davideceolin <davide.ceolin@gmail.com>
2018-09-07 12:07:19 +0200

    app.py fix for heroku

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ from sub import api
 import os
 from run import app as application
 if __name__ == '__main__':
+    application = Flask(__name__)
     api.init_app(application)
     port = int(os.environ.get('PORT', 5000))
     application.run(host='0.0.0.0', port=port)
+
+
+#app = Flask(__name__)
+#app.wsgi_app = ProxyFix(app.wsgi_app)
+
+#api.init_app(app)
+
+#app.run(debug=True)
