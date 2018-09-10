7573eea31a90bf935eadf4011e8403763f633fdc
davideceolin <davide.ceolin@gmail.com>
2018-09-10 16:20:35 +0200

    app.py fix for heroku

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from sub import api
 
 import os
 
+app = Flask(__name__)
+
 if __name__ == '__main__':
-    app = Flask(__name__)
     api.init_app(app)
 #port = int(os.environ.get('PORT', 5000))
     app.run()#host='0.0.0.0')#, port=port)
